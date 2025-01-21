7e271361cda250f51fbe56c11ba489d491a7fdb0
Virat <viratmpriya@yahoo.com>
2025-01-21 12:48:49 -0800

    Create main script.js

---
+++ main script.js
@@ -0,0 +1,10 @@
+var input = prompt("Welcome, enter the number 38512 to continue","");
+if (input == 38512)
+alert("Correct, you can now continue.");
+
+else
+alert("Wrong, you cannot continue.");
+
+do{
+alert ("");
+}while(1 == 1)
